976e531aff43631ac9fdfed5357eddc64133244b
Danchoo21 <60216437+SeongMyeongYun@users.noreply.github.com>
2022-01-23 17:28:51 +0900

    change gradleJvm -> 1.8

--- buildSrc/src/main/java/GradleExtension.kt
+++ buildSrc/src/main/java/GradleExtension.kt
@@ -8,8 +8,8 @@ object Config {
     const val BUILD_TOOL_VERSION = "31.0.0"
     const val MIN_SDK = 21
     const val TARGET_SDK = 31
-    const val VERSION_NAME = "1.0.9"
-    const val VERSION_CODE = 100009
+    const val VERSION_NAME = "1.0.10"
+    const val VERSION_CODE = 100010
 
     const val TEST_INSTRUMENTATION_RUNNER = "com.danchoo.date.CustomTestRunner"
 
