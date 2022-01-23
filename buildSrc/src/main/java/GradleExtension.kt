ea5be1b9a9b70e53c00dfacb4a0300653a7567e4
danchoo21 <danchoo45@naver.com>
2022-01-23 15:35:10 +0900

    1.0.2

--- buildSrc/src/main/java/GradleExtension.kt
+++ buildSrc/src/main/java/GradleExtension.kt
@@ -8,8 +8,8 @@ object Config {
     const val BUILD_TOOL_VERSION = "31.0.0"
     const val MIN_SDK = 21
     const val TARGET_SDK = 31
-    const val VERSION_NAME = "1.0.1"
-    const val VERSION_CODE = 100001
+    const val VERSION_NAME = "1.0.2"
+    const val VERSION_CODE = 100002
 
     const val TEST_INSTRUMENTATION_RUNNER = "com.danchoo.date.CustomTestRunner"
 
